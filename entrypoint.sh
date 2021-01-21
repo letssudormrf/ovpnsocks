c22a112c3a82d31001faed300a457235117dd618
letssudormrf <letssudormrf@users.noreply.github.com>
2021-01-21 23:10:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+# export RELAY_TARGET=${RELAY_TARGET:-"squid.example.com:3128"}
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
-#socat tcp6-listen:8080,reuseaddr,fork tcp4:squid.example.com:3128 &
+if [ ! -z "$RELAY_TARGET" ]; then
+    socat tcp6-listen:8080,reuseaddr,fork tcp4:${RELAY_TARGET} &
+fi
 openvpn --config /tmp/${CONFIG} --script-security 2 --up /etc/openvpn/up.sh --down /etc/openvpn/down.sh
