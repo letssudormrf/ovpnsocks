6e262e5f64ca714be880956f4c695180f4e208c2
letssudormrf <letssudormrf@users.noreply.github.com>
2021-01-21 23:03:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
+#socat tcp6-listen:8080,reuseaddr,fork tcp4:squid.example.com:3128
 openvpn --config /tmp/${CONFIG} --script-security 2 --up /etc/openvpn/up.sh --down /etc/openvpn/down.sh
